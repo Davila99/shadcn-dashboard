cce280e161eff47feca38527765527c24727deed
Davila99 <davilaeliseo453@gmail.com>
2024-06-19 19:01:25 -0600

    button

--- src/app/dashboard/button/page.tsx
+++ src/app/dashboard/button/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 import { Button } from "@/components/ui/button";
+import { ChevronRightIcon, EnvelopeOpenIcon, ReloadIcon } from "@radix-ui/react-icons";
+
 
 export default function Page() {
   return (
@@ -13,6 +15,18 @@ export default function Page() {
       <Button disabled>disabled</Button>
       <Button variant={'success'}>disabled</Button>
       <Button onClick={() => console.log("Hola Mundo")}>Click Me</Button>
+      <Button capitalize={false}>capitalize false</Button>
+      <Button variant="outline" size="icon">
+      <ChevronRightIcon className="h-4 w-4" />
+    </Button>
+    <Button>
+      <EnvelopeOpenIcon className="mr-2 h-4 w-4" /> Login with Email
+    </Button>
+    <Button disabled>
+      <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+      Please wait
+    </Button>
+ 
     </div>
   );
 }
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -39,14 +39,17 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean
+  capitalize?: boolean
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, asChild = false,capitalize = true, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(buttonVariants({ variant, size, className }),{
+          'capitalize': capitalize,
+        })}
         ref={ref}
         {...props}
       />
